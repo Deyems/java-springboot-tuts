0727bd1c12d23d6711a53cc033e3c0ccfcb3626a
Deyems <adeyemisola11@gmail.com>
2026-09-02 20:28:41 +0100

    inheritance

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -9,66 +9,80 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //    int marks;
 //}
 
-class Mobile {
-    String brand;
-    String network;
-    int price;
-    static String name;
+//class Mobile {
+//    String brand;
+//    String network;
+//    int price;
+//    static String name;
+//
+//    static {
+//        name = "Phone";
+//        System.out.println("Inside the static block");
+//    }
+//
+//    public Mobile(){
+//        brand = "";
+//        price = 200;
+//        System.out.println("Inside the constructor");
+//    }
+//
+//    void show(){
+//        System.out.println(brand + " : " + price + " :" + name);
+//    }
+//
+//    public static void show1(){
+//        System.out.println("added static method in mobille");
+////        System.out.println(brand + " " + price + "last & name" + name);
+//    }
+//}
+//
+//class Human {
+//    private int age;
+//    private String name = "Adesola";
+//
+//    // Normal constructor
+//    public Human(){
+//        age = 12;
+//        name = "Default";
+//        System.out.println("values in the human constructor!");
+//    }
+//
+//    // Parameterized constructor
+//    public Human(int a, String n){
+//        age = a;
+//        name = n;
+//    }
+//
+//    public int getAge(){
+//        return age;
+//    }
+//
+//    public void setAge(int age){
+////        Human obj1 = new Human();
+////        Human obj1 = obj;
+//        this.age = age;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//}
 
-    static {
-        name = "Phone";
-        System.out.println("Inside the static block");
-    }
-
-    public Mobile(){
-        brand = "";
-        price = 200;
-        System.out.println("Inside the constructor");
-    }
-
-    void show(){
-        System.out.println(brand + " : " + price + " :" + name);
-    }
-
-    public static void show1(){
-        System.out.println("added static method in mobille");
-//        System.out.println(brand + " " + price + "last & name" + name);
+//use of extends and super.
+//inheritance
+class A {
+    public A(){
+        System.out.println("In Class A");
     }
 }
 
-class Human {
-    private int age;
-    private String name = "Adesola";
-
-    // Normal constructor
-    public Human(){
-        age = 12;
-        name = "Default";
-        System.out.println("values in the human constructor!");
-    }
-
-    // Parameterized constructor
-    public Human(int a, String n){
-        age = a;
-        name = n;
-    }
-
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age){
-//        Human obj1 = new Human();
-//        Human obj1 = obj;
-        this.age = age;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
+class B extends A {
+    public B(){
+        System.out.println("In Class B");
     }
 }
 
@@ -80,13 +94,15 @@ public class StoreApplication {
     // int nums [][] = new int[3][4];
     // using class to load a class
     // Class.forName("com.codewithmosh.store.Mobile");
-        Human humanObj = new Human();
-//        humanObj.setAge(23);
-//        humanObj.setName("Mavin Beacon");
-        Human humanObj_2 = new Human(18, "Navin");
-        System.out.println(humanObj.getName() + " " + "Your age is: " + humanObj.getAge());
-        System.out.println("Normal Constructor vs Parameterized Constructor ");
-        System.out.println(humanObj_2.getName() + " " + "Your age is: " + humanObj_2.getAge());
+//        Human humanObj = new Human();
+////        humanObj.setAge(23);
+////        humanObj.setName("Mavin Beacon");
+//        Human humanObj_2 = new Human(18, "Navin");
+//        System.out.println(humanObj.getName() + " " + "Your age is: " + humanObj.getAge());
+//        System.out.println("Normal Constructor vs Parameterized Constructor ");
+//        System.out.println(humanObj_2.getName() + " " + "Your age is: " + humanObj_2.getAge());
+
+        B obj = new B();
 
     }
 }
