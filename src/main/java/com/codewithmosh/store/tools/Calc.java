8c3df90100eec671de7122cfe40caa96fd158400
Deyems <adeyemisola11@gmail.com>
2026-09-08 22:17:12 +0100

    refactored classes into packages

---
+++ src/main/java/com/codewithmosh/store/tools/Calc.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.tools;
+
+public class Calc {
+    int add(int n, int m){
+        return n + m;
+    }
+
+    int sub(int big, int small){
+        return big - small;
+    }
+}
