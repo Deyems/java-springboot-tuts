5a5718eb3220d8158356eca8225e8051a41b6ceb
Deyems <adeyemisola11@gmail.com>
2026-09-26 13:36:43 +0100

    using switch cases with enum

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -11,11 +11,25 @@ enum Status {
 public class StoreApplication {
 
     public static void main(String[] args) {
-        Status s = Status.Pending;
-        System.out.println(s + ": check the status printed in the enum");
-        Status [] states = Status.values();
-        for (Status state : states){
-            System.out.println("Status " + state + ": and the rank is " + state.ordinal());
+        Status s = Status.Failed;
+//        System.out.println(s + ": check the status printed in the enum");
+//        Status [] states = Status.values();
+//        for (Status state : states){
+//            System.out.println("Status " + state + ": and the rank is " + state.ordinal());
+//        }
+
+        switch(s) {
+            case Running:
+                System.out.println("All Good");
+                break;
+            case Pending:
+                System.out.println("Please Wait");
+                break;
+            case Failed:
+                System.out.println("Try Again");
+                break;
+            default:
+                System.out.println("Done");
         }
 
     }
