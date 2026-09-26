d958ef8bcc8acbafa1aabfd2b4d744e7d89213ae
Deyems <adeyemisola11@gmail.com>
2026-09-26 17:54:37 +0100

    using enum constructors

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -3,34 +3,37 @@ package com.codewithmosh.store;
 import com.codewithmosh.store.tools.AdvCalc;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-enum Status {
-    Pending, Running, Failed, Success
+enum Laptop {
+
+    Mac(2000), lenovo(1800), hewlettPackard(1500), xps(1200);
+
+    private int price;
+
+    Laptop(int price){
+        this.price = price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
 }
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        Status s = Status.Failed;
-//        System.out.println(s + ": check the status printed in the enum");
-//        Status [] states = Status.values();
-//        for (Status state : states){
-//            System.out.println("Status " + state + ": and the rank is " + state.ordinal());
-//        }
+        //Enum Constructor.
+        Laptop laptop = Laptop.Mac;
+        System.out.println(laptop + " goes for N" + laptop.getPrice());
 
-        switch(s) {
-            case Running:
-                System.out.println("All Good");
-                break;
-            case Pending:
-                System.out.println("Please Wait");
-                break;
-            case Failed:
-                System.out.println("Try Again");
-                break;
-            default:
-                System.out.println("Done");
+        //printing all enums below:
+        for(Laptop eachLaptop : Laptop.values()){
+            System.out.println("this brand of laptop called " + eachLaptop + " costs about " + eachLaptop.getPrice() + " what is the name too? " + eachLaptop
+                    .name());
         }
-
     }
 }
